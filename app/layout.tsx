111a16ca93d4310c887b0f50d5ee9348f311015f
Dare zayan. <143078072+darezyan@users.noreply.github.com>
2025-09-03 21:25:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,35 +35,7 @@ export const metadata: Metadata = {
     "frontend developer portfolio",
     "creative developer portfolio",
   ],
-  colorScheme: "dark",
-  openGraph: {
-    title: "Victor Williams — Frontend Engineer",
-    description:
-      "Frontend Engineer and Web Designer, currently at Kora. Focused on interfaces and experiences, working remotely from Lagos, Nigeria.",
-    url: "https://www.victorwilliams.me/",
-    siteName: "www.victorwilliams.me",
-    images: [
-      {
-        url: "https://user-images.githubusercontent.com/84178696/228620835-e3cc5c9b-72fc-4f54-a628-407ef7b650f5.png",
-        width: 1200,
-        height: 630,
-        alt: "Victor Williams — Frontend Engineer",
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Victor Williams — Frontend Engineer",
-    description:
-      "Frontend Engineer and Web Designer, currently at Kora. Focused on interfaces and experiences, working remotely from Lagos, Nigeria.",
-    creator: "victorwill__",
-    creatorId: "1243720976552144897",
-    images: [
-      "https://user-images.githubusercontent.com/84178696/228620835-e3cc5c9b-72fc-4f54-a628-407ef7b650f5.png",
-    ],
-  },
+ 
   robots: {
     index: true,
     follow: true,
